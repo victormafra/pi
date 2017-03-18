fed0e70fe9a2c8b248e9487b529cc887fa4589bb
Victor Lucas de Melo Mafra <vlmmafra@gmail.com>
2017-03-18 18:02:05 -0300

    Update pi.cpp
    Code changes to use the new header (:

--- pi.cpp
+++ pi.cpp
@@ -1,24 +1,16 @@
 #include <iostream>
-#include <fstream>
-#include <string>
+#include "pi.h"
 using namespace std;
+
 
 int main(){
 	unsigned int i = 0, wrong = 0;
-	string pi = "14159265", line;
-	string input_history = "";
+	string pi = "14159265", fullInput = "";
 	char input;
 	
-	ifstream myfile ("pi.txt");
-	if (myfile.is_open()){
-		while ( getline (myfile,line) ){
-			pi = line;
-		}
-		myfile.close();
-	} else {
-		cout << "Unable to open file"; 
-	}
+	pi = copyPiFromFile("pi.txt");
 	
+	/* Input */ 
 	cout << "Start typing, when you're tired press enter." << endl << "3.";
 	
 	while(scanf("%c", &input)){
@@ -27,31 +19,17 @@ int main(){
 		}
 		if(input!=pi[i++])
 			wrong++;
-		input_history+=input;
+		fullInput+=input;
 	}
 	
+	/* Comparison Screen */
 	cout << endl << "Correct answer: " << endl;
-	cout << "3.";
-	for(int j=0; j<i; j++){
-		if(j%8==0 && j!=0){
-			cout << " ";
-		}
-		cout << pi[j];
-	}
-	cout << endl << endl;
-	
-	
+	piWithEightSpaces(pi,i);
 	cout << "Your answer: " << endl;
-	cout << "3.";
-	for(int j=0; j<i; j++){
-		if(j%8==0 && j!=0){
-			cout << " ";
-		}
-		cout << input_history[j];
-	}
-	cout << endl << endl << endl;
+	piWithEightSpaces(fullInput,i);
 	
-	cout << "You typed " << i << " decimal places." << endl << wrong << " of them are wrong." << endl << i-wrong << " of them are right." << endl;
+	/* Stats Screen */
+	cout << endl << "You typed " << i << " decimal places." << endl << wrong << " of them are wrong." << endl << i-wrong << " of them are right." << endl;
 	cout << endl << "And I love you <3" << endl;
 	
 	return 0;

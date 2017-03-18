60695c199c010608c4b082b0585a22d1898cdb29
Victor Lucas de Melo Mafra <vlmmafra@gmail.com>
2017-03-18 18:01:27 -0300

    Add files via upload
    I included a header with functions to make code cleaner

---
+++ pi.h
@@ -0,0 +1,31 @@
+#ifndef PI.H
+#define PI.H
+
+#include <fstream>
+using namespace std;
+string copyPiFromFile(string fileName){
+	string pi, line;
+	ifstream myfile (fileName);
+	if (myfile.is_open()){
+		while ( getline (myfile,line) ){
+			pi = line;
+		}
+		myfile.close();
+	} else {
+		cout << "Unable to open file"; 
+	}
+	return pi;
+}
+
+void piWithEightSpaces(string s, int size){
+	cout << "3.";
+	for(int j=0; j<size; j++){
+		if(j%8==0 && j!=0){
+			cout << " ";
+		}
+		cout << s[j];
+	}
+	cout << endl;
+}
+
+#endif 
